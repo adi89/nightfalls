03823267cd7e6392751e309dc99773595135a130
Adi Singh <adityasingh89@gmail.com>
2014-01-16 16:39:31 -0500

    variables in rake tasks can't be accessed elsewhere

--- config/initializers/train_sample.rb
+++ config/initializers/train_sample.rb
@@ -1,1 +1,12 @@
-CLASSIFIER = StuffClassifier::Bayes.new("night or discard")
+   CLASSIFIER = StuffClassifier::Bayes.new("night or discard")
+    # CLASSIFIER.ignore_words = [ 'the', 'my', 'i', 'dont' ]
+    irrelevant = Tweet.collect_strings(Tweet.irrelevant)
+    nightlife = Tweet.collect_strings(Tweet.nightlife)
+
+    nightlife.each do |tweet|
+      CLASSIFIER.train(:night, tweet)
+    end
+
+    irrelevant.each do |tweet|
+      CLASSIFIER.train(:discard, tweet)
+    end
