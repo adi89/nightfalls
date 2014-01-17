1ddec368d082b18ef0cf8b6c6c6bcd55ec0f84f9
Adi Singh <adityasingh89@gmail.com>
2014-01-17 14:40:23 -0500

    more log info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def follow
     current_user.friends << Friend.where(username: params['username'])
     Friend.follow_user({username: params['username'], token: current_user.token, token_secret: current_user.token_secret})
+    puts "#{current_user.token}"
+    puts "#{current_user.token_secret}"
     #client follows
     render nothing: true, layout: false
   end
