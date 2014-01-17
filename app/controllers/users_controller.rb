02d509f2d88006d1f0497129d00a78a9e8b4a0c9
Adi Singh <adityasingh89@gmail.com>
2014-01-17 15:01:20 -0500

    examinig logs, testing devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,10 @@ class UsersController < ApplicationController
 
   def follow
     current_user.friends << Friend.where(username: params['username'])
-    Friend.follow_user({username: params['username'], token: current_user.token, token_secret: current_user.token_secret})
     puts "#{current_user.token}"
     puts "#{current_user.token_secret}"
+    puts "#{current_user.id}"
+    Friend.follow_user({username: params['username'], token: current_user.token, token_secret: current_user.token_secret})
     #client follows
     render nothing: true, layout: false
   end
