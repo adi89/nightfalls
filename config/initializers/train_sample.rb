bf668e5265c64467be6d5ff7ea2258ebc8c9c939
Adi Singh <adityasingh89@gmail.com>
2014-01-20 19:20:25 -0500

    redis storage

--- config/initializers/train_sample.rb
+++ config/initializers/train_sample.rb
@@ -1,9 +1,9 @@
 require 'redis'
     @key = "stuff-classifier"
-    # @redis_options = { host: 'http://redistogo:9e2a2e377715c850a14297f8a107736e@crestfish.redistogo.com', port: 10559 }
+    @redis_options = { host: 'http://redistogo:9e2a2e377715c850a14297f8a107736e@crestfish.redistogo.com', port: 10559 }
 
-    # @storage = StuffClassifier::RedisStorage.new(@key, @redis_options)
-    # StuffClassifier::Base.storage = @storage
+    @storage = StuffClassifier::RedisStorage.new(@key, @redis_options)
+    StuffClassifier::Base.storage = @storage
 
     # @redis_options = { host: 'localhost', port: 6379 }
     # redis = Redis.new(@redis_options)
